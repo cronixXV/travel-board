79e0243b0d8da9ca124ca48d1d186f5d1b9fd57f
Egor Gruzdev <egogryzdev@team.amocrm.com>
2026-06-03 23:09:33 +0300

    fix

--- server/src/database/config.js
+++ server/src/database/config.js
@@ -2,18 +2,24 @@ require('dotenv').config({
   path: require('path').resolve(process.cwd(), '../.env'),
 });
 
+const sslOptions = {
+  require: true,
+  rejectUnauthorized: false,
+};
+
 module.exports = {
   development: {
     url: process.env.DATABASE_URL,
     dialect: 'postgres',
     logging: false,
   },
+
   production: {
     url: process.env.DATABASE_URL,
     dialect: 'postgres',
     logging: false,
     dialectOptions: {
-      ssl: { require: true, rejectUnauthorized: true },
+      ssl: sslOptions,
     },
   },
 };
